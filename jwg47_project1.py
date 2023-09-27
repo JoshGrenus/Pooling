d482247f980643643eb4381e9623f17bc64ce6a4
JoshGrenus <122390440+JoshGrenus@users.noreply.github.com>
2023-09-27 12:58:48 -0400

    Add files via upload

---
+++ jwg47_project1.py
@@ -0,0 +1,46 @@
+# Big Data Programming Project 1
+# Author: your name
+import sys
+import numpy as np
+
+
+# @param: .pgm filename
+# @return: a tuple (header, image array) # image array - numpy array
+def pgmFileRead(fileName):
+    img_header = []   # a place holder
+    img = np.array([])   # a place holder
+
+    return img_header, img
+
+
+# @param: image array; image header; name of the processed image
+# return: NaN
+def image_save(output_header, image_array, fileName):
+    return None
+
+
+# @param: image array, pool size
+# @return: pooled array
+def max_pooling(input_array, pool_size):
+    pooled_array = np.array([])   # a place holder
+
+    return pooled_array
+
+
+# @param: image array, pool size
+# @return: oil painted image array
+def oil_painting(input_array, pool_size):
+    oil_array = np.array([])  # a place holder
+
+    return oil_array
+
+
+def main():
+    imgFileName, poolSize, part = sys.argv[1:]
+    print (imgFileName)
+    print (poolSize)
+    print (part)
+
+
+if __name__ == '__main__':
+    main()
