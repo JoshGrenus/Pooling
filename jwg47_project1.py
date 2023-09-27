ba66cdca3e3d4fe589098bbca8c91225144664a7
Josh Grenus <jwg47@uakron.edu>
2023-09-27 19:42:06 -0400

    trying to get from list to np array

--- jwg47_project1.py
+++ jwg47_project1.py
@@ -7,6 +7,7 @@ import numpy as np
 # @param: .pgm filename
 # @return: a tuple (header, image array) # image array - numpy array
 def pgmFileRead(fileName):
+    h = w = col = row = 0
     file = open(fileName, 'r')
     img_header = []   # a place holder
     img = np.array([])   # a place holder
@@ -24,20 +25,28 @@ def pgmFileRead(fileName):
             img_header.remove(item)
     pgmSize = img_header[1].split(" ")
     height = pgmSize[0], width = pgmSize[1]
-    
+
     for line in file:
         imgTmp.append(line.strip())
     for x in imgTmp:
         allVals = allVals + " " + x
+    imgTmp.clear()
     allVals = allVals.split(" ")
-    allVals.remove(' ')
-
-
+    allVals.remove('')
+     
+    for item in allVals:
+        if (col == width):
+            col = 0
+            img = np.concatenate((img, imgTmp), axis=0)
+            row += 1
+            break
+        imgTmp.append(item)
+        col += 1
     
     print(allVals)
-    print (img_header)
-    print ('\n')
-    print (img)
+    print(img_header)
+    print('\n')
+    print(img)
 
     file.close()
     return img_header, img
