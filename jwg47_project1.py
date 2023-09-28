dd92b380d97b51c0e2664e7b0befb0d41da1903e
Josh Grenus <jwg47@uakron.edu>
2023-09-28 11:47:49 -0400

    Works with bug/flower, need to get bug2 to work

--- jwg47_project1.py
+++ jwg47_project1.py
@@ -7,43 +7,43 @@ import numpy as np
 # @param: .pgm filename
 # @return: a tuple (header, image array) # image array - numpy array
 def pgmFileRead(fileName):
-    h = w = col = row = 0
+    height = width = col = row = 0
+    y = 5
     file = open(fileName, 'r')
     img_header = []   # a place holder
     img = np.array([])   # a place holder
     imgTmp = []     #temp array to hold pmg values
     allVals = ""
 
-    for line in file:
-        if (len(line)<50):
-            img_header.append(line.strip())
+    for line in file:                               #Loops through to get the header set up, stops after getting max num value.
+        
+        if (len(line) == 4):
+           img_header.append(line.strip())
+           break
         else:
-            break
+           img_header.append(line.strip())
 
     for item in img_header:
         if (item.startswith('#')):
             img_header.remove(item)
     pgmSize = img_header[1].split(" ")
-    height = pgmSize[0], width = pgmSize[1]
+    height = int(pgmSize[0])
+    width = int(pgmSize[1])
+    img = np.zeros((height,width))
 
     for line in file:
+
         imgTmp.append(line.strip())
+        y += 1
+
     for x in imgTmp:
         allVals = allVals + " " + x
+
     imgTmp.clear()
     allVals = allVals.split(" ")
     allVals.remove('')
-     
-    for item in allVals:
-        if (col == width):
-            col = 0
-            img = np.concatenate((img, imgTmp), axis=0)
-            row += 1
-            break
-        imgTmp.append(item)
-        col += 1
-    
-    print(allVals)
+    img = np.array(allVals).reshape(height,width)
+
     print(img_header)
     print('\n')
     print(img)
@@ -77,9 +77,7 @@ def oil_painting(input_array, pool_size):
 
 def main():
     imgFileName, poolSize, part = sys.argv[1:]
-    print (imgFileName)
-    print (poolSize)
-    print (part)
+
     pgmFileRead(imgFileName)
 
 
